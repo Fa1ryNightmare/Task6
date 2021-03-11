3c1eee41950fce432e24cbab7f3dcba15e8a4e97
Sukharevich Vladislav <vladoslav.sukharevich3@gmail.com>
2021-03-11 11:17:53 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main()
+        {
+            int N = 3;
+            int[,] A = new int[N, N];
+            using(StreamReader sr = new StreamReader("Inlet.in"))
+            {
+                Console.WriteLine("Считаная матрица: ");
+                for(int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        A[i, j] = int.Parse(sr.ReadLine().Trim());
+                        Console.Write($"{A[i, j]} ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            int mainDia = 0;
+            for (int i = 0; i < N; i++)
+            {
+                mainDia += A[i, i];
+            }
+            int secondaryDia = 0;
+            for (int i = 0, j = N - 1; i < N; i++, j--)
+            {
+                secondaryDia += A[i, j];
+            }
+            string result = $"Сумма главной диагонали: {mainDia}\nСумма впомогательной диагонали: {secondaryDia}";
+            File.WriteAllText("Outlet.out", result);
+        }
+    }
+}
